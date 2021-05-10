c7f4185a5499063063ba749b75649c33531dece2
JJSanch <jon.sanchez003@gmail.com>
2021-05-10 09:55:53 -0700

    iniital assessment commit

--- coding-concepts.js
+++ coding-concepts.js
@@ -1,51 +1,44 @@
-// ASSESSMENT 1: Coding conceptual questions
-
-// Examine the following examples.
-
-// First, predict the outcome based on your understanding of the code.
-// Then, uncomment the console.log() and verify the output. Briefly explain why your initial answer was correct or incorrect.
-
 // --------------------1) What will this log?
 
-var cohort = "Bravo 2021"
+// var cohort = "Bravo 2021"
 // console.log(cohort.length)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: It will log the index numbers of the string
+// b) Verify and explain: "10" was logged as there are 10 total indexes corresponding to each letter and number
 
 
 // --------------------2) What will this log?
 
-var greeting = "Hello World!"
+// var greeting = "Hello World!"
 // console.log(greeting[3])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: This will log the letter "l" as the console will log the third index which belongs to "l"
+// b) Verify and explain: 'l' was logged as it was in the third index
 
 
 // --------------------3) What will this log?
 
-var languages = ["JavaScript", "Ruby", "Python", "C++"]
-var index = 1
+// var languages = ["JavaScript", "Ruby", "Python", "C++"]
+// var index = 1
 // console.log(languages[index])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: This will log "Ruby" as its in the first index of the string
+// b) Verify and explain: "Ruby" was logged as its in the first index of the string above
 
 
 // --------------------4) What will this log?
 
-var weekendDays = ["saturday", "sunday"]
+// var weekendDays = ["saturday", "sunday"]
 // console.log(weekendDays.toUpperCase())
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: This will convert the above days in the string into all upper case letters
+// b) Verify and explain: Nothing logged as terminal indicates that (weekendDays.toUpperCase()) is not a function
 
 
 // --------------------5) What will this log?
 
-var dataTypes = ["number", "string", "Boolean"]
+// var dataTypes = ["number", "string", "Boolean"]
 // console.log(typeof dataTypes.length)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: This will log the index length of each variable
+// b) Verify and explain: This logged "number."  COuld not determine why it returned this.
